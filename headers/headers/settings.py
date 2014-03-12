95bf49e6c31a7d46b0a7843d7cd79d0bee80cb55
Jimmy <jimmy.mesta@gmail.com>
2014-03-12 02:17:24 +0000

    getting postgresql up and working

--- headers/headers/settings.py
+++ headers/headers/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'headersapp'
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +60,12 @@ WSGI_APPLICATION = 'headers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'headers',
+	'USER': 'headers',
+        'PASSWORD': 'Fitbikerider143!',
+        'HOST': 'localhost',
+        'PORT': '', 
     }
 }
 
